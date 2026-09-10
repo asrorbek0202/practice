747230fc4e335e53f6a995334d81512b02ef6063
Steve <asrorbekmannobjonov230@gmail.com>
2026-09-11 05:46:08 +0900

    feat: learning string in python

--- primitive.py
+++ primitive.py
@@ -9,3 +9,20 @@ print(f" the count: {count}, and type: {count_type}")
 result1 = count.bit_count()  # method
 result2 = count.numerator  # state property
 print(f"result1: {result1}, result2: {result2}")
+
+
+print("============= string ===================")
+# METHODS : upper(), lower(), title(), capitalize(), swapcase(), replace(), isupper(), islower(), istitle(), isspace()
+
+course = "AI Python FullStack"
+result = type(course)
+print(f"the result(1) : {result}")
+
+result = course.title()
+print(f"the result(2) : {result}")
+
+result = course.upper()
+print(f"the result(3) : {result}")
+
+result = course.replace("FullStack", "MasterClass")
+print(f"the result(4) : {result}")

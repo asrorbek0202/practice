bd158a58d6ac56625daa18a6802b0458bc54a872
Steve <asrorbekmannobjonov230@gmail.com>
2026-09-13 05:09:32 +0900

    fix: pyton special/magic methods

--- class.about.py
+++ class.about.py
@@ -50,3 +50,47 @@ print("new_messeage", new_messeage)
 
 # static method
 Person.explain()
+
+
+print("=== special methods of class ====")
+# special methods are also called magic methods or dunder methods
+# __init__(), __str__(), __repr__(), __len__(), __add__(), __sub__(), __mul__(), __truediv__(), __eq__(), __ne__(), __lt__(), __le__(), __gt__(), __ge__()
+
+
+class Car():
+    # state
+    description = "This class makes cars"
+
+    # constructor
+    def __new__(cls, *args):
+        print("*__new__*")
+        return super().__new__(cls)
+
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+
+    # methods
+    def start_engine(self):
+        print(f"the {self.name} started engine !")
+
+    def stop_engine(self):
+        print(f"the {self.name} stopped engine!")
+
+    def __str__(self):
+        return f"the car.name: {self.name} was produced in {self.year} year!"
+
+    def __call__(self):
+        print("object called as a function")
+        return True
+
+
+my_car = Car("Ferrari", 2025)
+my_car.start_engine()
+my_car.stop_engine()
+
+print("----")
+your_car = Car("Tayota", 2026)
+print(your_car)
+response = your_car()
+print("response", response)
